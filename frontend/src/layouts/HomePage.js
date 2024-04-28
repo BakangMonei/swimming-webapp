c2d1acd80aa1f569177411089ffb9fa863e7cdc3
BakangMonei <bakangmonei2@gmail.com>
2024-04-28 16:01:58 +0200

    dixajb

--- frontend/src/layouts/HomePage.js
+++ frontend/src/layouts/HomePage.js
@@ -2,14 +2,19 @@ import React from 'react';
 import NavBar from '../components/navbar/NavBar';
 import SliderShow from '../components/slideshow/SliderShow';
 import HomeFooter from '../components/footer/HomeFooter';
+import Carousell from '../components/carousel/Carousel';
 
 
 
 const HomePage = () => {
   return (
-    <div className="max-h-screen">
+    <div className="max-h-screen ">
       <NavBar />
-      <SliderShow />
+      {/* <SliderShow /> */}
+
+      <div className="max-h-screen">
+      <Carousell/>
+      </div>
       <HomeFooter />
     </div>
   );
